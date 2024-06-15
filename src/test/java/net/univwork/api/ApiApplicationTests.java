78521702ad97768124e49d93063e46f3af9093a0
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 16:53:10 +0900

    Initial Commit

---
+++ src/test/java/net/univwork/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.univwork.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
