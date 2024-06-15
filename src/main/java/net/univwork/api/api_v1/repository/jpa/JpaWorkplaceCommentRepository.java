4851f5a0264c6d5e4bb19c4e7da09aa4d706f33d
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:16:58 +0900

    JpaWorkplaceCommentRepository

---
+++ src/main/java/net/univwork/api/api_v1/repository/jpa/JpaWorkplaceCommentRepository.java
@@ -0,0 +1,23 @@
+package net.univwork.api.api_v1.repository.jpa;
+
+import net.univwork.api.api_v1.domain.entity.WorkplaceComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface JpaWorkplaceCommentRepository extends JpaRepository<WorkplaceComment, Long> {
+
+    /**
+     * 학교코드와 근로지 코드로 근로지 댓글들을 최근 순으로 정렬하여 가져오는 메소드
+     * @param pageable 페이지에이블
+     * @param univCode 학교 코드
+     * @param workplaceCode 근로지 코드
+     * @return WorkplaceComment 가 담긴 페이지 객체
+     * @since 1.0.0
+     * */
+    @Query("SELECT wc FROM WorkplaceComment wc WHERE wc.univCode = :univCode AND wc.workplaceCode = :workplaceCode AND wc.deleteFlag = false ORDER BY wc.timestamp DESC")
+    Page<WorkplaceComment> getWorkplaceComments(Pageable pageable, @Param("univCode") Long univCode, @Param("workplaceCode") Long workplaceCode);
+
+}
