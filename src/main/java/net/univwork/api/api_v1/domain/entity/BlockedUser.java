bc029f8db536ceebc6e1a760c3ccb6eb164da650
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:12:52 +0900

    BlockedUser

---
+++ src/main/java/net/univwork/api/api_v1/domain/entity/BlockedUser.java
@@ -0,0 +1,20 @@
+package net.univwork.api.api_v1.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class BlockedUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sequence;
+
+    private String blockedUser;
+
+    private String reason;
+}
