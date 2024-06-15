e7bc0bea87e77b13fecf07d391fbd9e9d2372eb2
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:16:20 +0900

    InterceptorConfig

---
+++ src/main/java/net/univwork/api/api_v1/interceptor/config/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package net.univwork.api.api_v1.interceptor.config;
+
+import lombok.RequiredArgsConstructor;
+import net.univwork.api.api_v1.interceptor.UserCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final UserCheckInterceptor userCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userCheckInterceptor)
+                .addPathPatterns("/api/**")
+                .order(1);
+    }
+}
