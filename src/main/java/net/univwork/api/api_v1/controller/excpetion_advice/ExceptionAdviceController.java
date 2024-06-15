f8d629a28f36a17614806724fe03023587093b4b
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:16:11 +0900

    ExceptionAdviceController

---
+++ src/main/java/net/univwork/api/api_v1/controller/excpetion_advice/ExceptionAdviceController.java
@@ -0,0 +1,102 @@
+package net.univwork.api.api_v1.controller.excpetion_advice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.domain.response.ErrorResultAndMessage;
+import net.univwork.api.api_v1.exception.BlockedClientException;
+import net.univwork.api.api_v1.exception.NoUserCodeException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Locale;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdviceController {
+
+    private final MessageSource ms; // message.properties 접근용
+
+    /**
+     * API 예외 처리 핸들러_MethodArgumentTypeMismatchException
+     * @param e MethodArgumentTypeMismatchException
+     * @return ErrorResultAndMessage
+     * @see ErrorResultAndMessage
+     * @apiNote HTTP 요청 시 Method Parameter 에 잘못된 타입의 인자를 전달한 경우 MethodargumentTypeMismatchException 발생
+     * */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ErrorResultAndMessage methodArgTypeMis(MethodArgumentTypeMismatchException e) {
+        log.debug("[exceptionHandler] ex", e);
+        // messageSource 에서 message 획득, 틀린 파라미터 인덱스와 파라미터 타입
+        String message = ms.getMessage("exception.MethodArgumentTypeMismatchExceptionMessage", new Object[]{e.getParameter().getParameterIndex(), e.getParameter().getParameterType()}, Locale.KOREA);
+
+        log.debug("message={}", message);
+        return new ErrorResultAndMessage(HttpStatus.BAD_REQUEST.getReasonPhrase(), message);
+    }
+
+    /**
+     * API 예외처리 핸들러_IllegalArgumentException
+     * @param e IllegalArgumentException
+     * @return ErrorResultAndMessage
+     * @apiNote 잘못된 인자가 전달된 경우 IllgalArgumentException 발생
+     * @see ErrorResultAndMessage
+     * */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResultAndMessage illegalArgEx(IllegalArgumentException e) {
+        log.debug("IllegalExMessage={}", e.getMessage());
+
+        return new ErrorResultAndMessage(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+    }
+
+    /**
+     * API 예외처리 핸들러_NoUserCodeException
+     * @param e NoUserCodeException
+     * @return ErrorResultAndMessage
+     * @apiNote POST, PUT, PATCH, DELETE HTTP 요청 시 USER_COOKIE 가 없을 경우 <br>AOP @Around 에서 발생
+     * @see net.univwork.api.api_v1.aop.UserCheckAop AOP 발생 예시
+     * @see ErrorResultAndMessage
+     * */
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(NoUserCodeException.class)
+    public ErrorResultAndMessage noUserCode(NoUserCodeException e) {
+        log.debug("NoUserCodeExMessage={}", e.getMessage());
+
+        return new ErrorResultAndMessage(HttpStatus.FORBIDDEN.getReasonPhrase(), e.getMessage());
+    }
+
+    /**
+     * API 예외처리 핸들러_BlockedClientException
+     * @param e BlockedClientException
+     * @return ErrorResultAndMessage
+     * @apiNote POST, PUT, PATCH, DELETE HTTP 요청 시 차단된 uuid 또는 ip 인 경우<br>AOP @Around 에서 발생
+     * @see co.kr.univwork.univwork.aop.UserCheckAOP#userCheck(ProceedingJoinPoint) AOP 발생 예시
+     * @see ErrorResultAndMessage
+     * */
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(BlockedClientException.class)
+    public ErrorResultAndMessage blockedCatch(BlockedClientException e) {
+        log.debug("BlockedClientException={}", e.getMessage());
+        return new ErrorResultAndMessage(HttpStatus.FORBIDDEN.getReasonPhrase(), e.getMessage());
+    }
+
+    /**
+     * API 예외처리 핸들러_범용_RuntimeException
+     * @param e RuntimeException
+     * @return ErrorResultAndMessage
+     * @apiNote RuntimeException 발생 시
+     * @see ErrorResultAndMessage
+     * */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorResultAndMessage runtimeException(RuntimeException e) {
+        log.debug("RuntimeException={}", e.getMessage());
+        return new ErrorResultAndMessage(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
+    }
+}
