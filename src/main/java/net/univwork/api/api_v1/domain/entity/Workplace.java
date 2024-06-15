e434713800029bb2498f635df56029f639aaa814
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:19:56 +0900

    Workplace

---
+++ src/main/java/net/univwork/api/api_v1/domain/entity/Workplace.java
@@ -0,0 +1,61 @@
+package net.univwork.api.api_v1.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Workplace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long workplaceCode;
+
+    @Column
+    private Long univCode;
+
+    @Column
+    private String univName;
+
+    @Column
+    private String workType;
+
+    @Column
+    private String workplaceType;
+
+    @Column
+    private String workplaceName;
+
+    @Column
+    private String workplaceAddress;
+
+    @Column
+    private String workTime;
+
+    @Column
+    private String workDay;
+
+    @Column
+    private String requiredNum;
+
+    @Column
+    private String preferredDepartment;
+
+    @Column
+    private String preferredGrade;
+
+    @Column
+    private String jobDetail;
+
+    @Column
+    private String note;
+
+    @Column
+    private Long views;
+
+    @Column
+    private Long commentNum;
+
+    public Workplace() {
+    }
+}
