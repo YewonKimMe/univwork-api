bc98ba1226689da13b598add9e99310b60c5f004
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:17:48 +0900

    SettingConst

---
+++ src/main/java/net/univwork/api/api_v1/tool/SettingConst.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.tool;
+
+public class SettingConst {
+    public static final String crossOriginEnableDevAddress = "http://localhost:3000";
+
+    public static final String currentCrossOriginEnableAddress = crossOriginEnableDevAddress; // 배표 시 실제 주소로 변경
+}
