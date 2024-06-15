963e6822900581247e6ed7150c37aa193fa1cf4b
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:17:29 +0900

    NoUserCodeException

---
+++ src/main/java/net/univwork/api/api_v1/exception/NoUserCodeException.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.exception;
+
+public class NoUserCodeException extends RuntimeException {
+    public NoUserCodeException(String message) {
+        super(message);
+    }
+}
