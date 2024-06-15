a1ff3e8f276caf3f5945f3572f0238202a8b9263
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:18:23 +0900

    University

---
+++ src/main/java/net/univwork/api/api_v1/domain/entity/University.java
@@ -0,0 +1,36 @@
+package net.univwork.api.api_v1.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long univCode;
+
+    @Column
+    private String univName;
+
+    @Column
+    private String univUuid;
+
+    @Column
+    private String establishment;
+
+    @Column
+    private String region;
+
+    @Column
+    private String academicSystem;
+
+    @Column
+    private Integer workplaceNum;
+
+    public University() {
+    }
+}
