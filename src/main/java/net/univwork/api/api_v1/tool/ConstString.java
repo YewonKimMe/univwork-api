83784b97e10c453693247e2c5eb16d8d9a15fc28
YewonKimMe <kimyewon030428@gmial.com>
2024-06-15 20:14:41 +0900

    ConstString

---
+++ src/main/java/net/univwork/api/api_v1/tool/ConstString.java
@@ -0,0 +1,54 @@
+package net.univwork.api.api_v1.tool;
+
+public class ConstString {
+
+    public static final String UNIV_NAME_ASC = "univAsc";
+
+    public static final String UNIV_NAME_DESC = "univDesc";
+
+    public static final String WORKPLACE_NAME_ASC = "workplaceAsc";
+
+    public static final String WORKPLACE_NAME_DESC = "workplaceDesc";
+
+    public static final String WORKPLACE_VIEW_ASC = "workplaceViewAsc";
+
+    public static final String WORKPLACE_VIEW_DESC = "workplaceViewDesc";
+
+    public static final String WORKPLACE_COMMENT_NUM_DESC = "workplaceCommentNumDesc";
+
+    public static final String WORKPLACE_COMMENT_NUM_ASC = "workplaceCommentNumAsc";
+
+    public static final String WORKPLACE_TYPE_IN = "교내";
+
+    public static final String WORKPLACE_TYPE_OUT = "교외";
+
+    public static String USER_COOKIE = "PFUCU_4FC2127GC12K41";
+
+    public static String USER_ADD_RESTRICT = "_GUAR_5FC2413CF34K62";
+
+    public static String REASON_SHIT = "욕설";
+
+    public static String REASON_REPEAT = "도배";
+
+    public static String REASON_FIGHT = "분쟁";
+
+    public static String REASON_AD = "광고";
+
+    public static String REASON_DEFAMATION = "명예훼손";
+
+    public static String REASON_SPREAD = "개인정보 유포";
+
+    public static String REASON_REPORTUSER = "신고 악용";
+
+    public static String ADMIN_REPORTED_DELETE = "delete";
+
+    public static String ADMIN_REPORTED_COMMENT_USER_BLOCK = "comment-user-block";
+
+    public static String ADMIN_REPORT_COMMENT_USER_IPBLOCK = "comment-user-ipblock";
+
+    public static String ADMIN_REPORT_REJECT = "reject";
+
+    public static String ADMIN_REPORT_REJECT_REPORT_USER_BLOCK = "reject-report-user-block";
+
+    public static String ADMIN_REPORT_REJECT_REPORT_USER_IPBLOCK = "reject-report-user-ipblock";
+}
