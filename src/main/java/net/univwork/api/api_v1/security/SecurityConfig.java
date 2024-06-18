8bd58cb4df5d16c4099fc145301c118eafbc09ca
YewonKimMe <kimyewon030428@gmial.com>
2024-06-18 20:35:11 +0900

    csrf 검사, cors 관련 설정, session 설정 추가

--- src/main/java/net/univwork/api/api_v1/security/SecurityConfig.java
+++ src/main/java/net/univwork/api/api_v1/security/SecurityConfig.java
@@ -1,43 +1,60 @@
 package net.univwork.api.api_v1.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/admin/**").authenticated()
-                )
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/**").permitAll()
-                )
-                .formLogin(
-                        Customizer.withDefaults()
-                )
-                .sessionManagement(
-                        sessionManagementConfig -> sessionManagementConfig
-                                .maximumSessions(1)
-                                .maxSessionsPreventsLogin(false)
-                )
-                .csrf(
-                        csrf -> csrf.disable()
-                )
-                .httpBasic(
-                        Customizer.withDefaults()
-                );
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf"); // csrf request 설정
+
+        http.securityContext((contextConfigurer) ->
+                contextConfigurer
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(
+                        new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                                config.setAllowedMethods(Collections.singletonList("*"));
+                                config.setAllowCredentials(true);
+                                config.setAllowedHeaders(Collections.singletonList("*"));
+                                config.setMaxAge(3600L);
+                                return config;
+                            }
+                        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler)
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/api/v1/admin/**").authenticated()
+                        .requestMatchers("/api/**", "/**").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
