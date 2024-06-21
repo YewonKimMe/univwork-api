21e1e608230f2c41dffd49d71223163bdaa1b0b9
YewonKimMe <kimyewon030428@gmial.com>
2024-06-22 07:42:01 +0900

    AuthorityLoggingFilterAfter

---
+++ src/main/java/net/univwork/api/api_v1/filter/AuthorityLoggingFilterAfter.java
@@ -0,0 +1,25 @@
+package net.univwork.api.api_v1.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+@Slf4j
+public class AuthorityLoggingFilterAfter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            log.info("[유저명: {}, 접속 시간: {}, 권한: {}]", authentication.getName(), new Timestamp(System.currentTimeMillis()), authentication.getAuthorities());
+        }
+        filterChain.doFilter(request, response);
+    }
+}
