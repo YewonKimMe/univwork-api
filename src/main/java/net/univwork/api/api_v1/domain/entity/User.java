c3e8ce9a64f565da28882c1237ce2901041a9456
YewonKimMe <kimyewon030428@gmial.com>
2024-06-22 07:44:03 +0900

    User

---
+++ src/main/java/net/univwork/api/api_v1/domain/entity/User.java
@@ -0,0 +1,36 @@
+package net.univwork.api.api_v1.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password")
+    private String pwd;
+
+    private String role;
+
+    @Column(name = "create_date")
+    private Timestamp createDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="user", fetch=FetchType.EAGER)
+    private Set<Authority> authorities;
+
+}
