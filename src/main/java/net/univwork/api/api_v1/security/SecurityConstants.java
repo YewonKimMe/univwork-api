51066c6c61cc67f358b34e4d26ed06ff73d468c4
YewonKimMe <kimyewon030428@gmial.com>
2024-06-23 10:44:15 +0900

    SecurityConstants

---
+++ src/main/java/net/univwork/api/api_v1/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package net.univwork.api.api_v1.security;
+
+public interface SecurityConstants {
+    public static final String JWT_KEY = "fSDFakrndkaSDmwekqrnCHfnfgkrEKqkeqoEKroRNFMaskdKEKQL";
+
+    public static final String JWT_HEADER = "Authorization";
+
+}
