f5cd2a68688d2ad4dae8a311dbe3e42a15fb0903
YewonKimMe <kimyewon030428@gmial.com>
2024-06-25 04:50:34 +0900

    @NotBlank message added

--- src/main/java/net/univwork/api/api_v1/domain/dto/SignUpIdDto.java
+++ src/main/java/net/univwork/api/api_v1/domain/dto/SignUpIdDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public class SignUpIdDto {
 
-    @NotBlank
+    @NotBlank(message = "아이디를 입력해 주세요.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$", message = "아이디는 최소 6자 이상으로, 영문자와 숫자를 포함해야 합니다.")
     private String id;
 }
