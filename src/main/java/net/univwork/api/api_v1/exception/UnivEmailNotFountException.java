7a472c069dbd2e977d13a4ead574ded967c94133
YewonKimMe <kimyewon030428@gmial.com>
2024-06-24 14:11:42 +0900

    UnivEmailNotFountException

---
+++ src/main/java/net/univwork/api/api_v1/exception/UnivEmailNotFountException.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.exception;
+
+public class UnivEmailNotFountException extends RuntimeException {
+    public UnivEmailNotFountException(String message) {
+        super(message);
+    }
+}
