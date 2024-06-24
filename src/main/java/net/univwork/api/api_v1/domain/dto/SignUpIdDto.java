798c3abcad3bacb5c1252358edb5b965aa67e13b
YewonKimMe <kimyewon030428@gmial.com>
2024-06-25 04:43:24 +0900

    SignUpIdDto

---
+++ src/main/java/net/univwork/api/api_v1/domain/dto/SignUpIdDto.java
@@ -0,0 +1,13 @@
+package net.univwork.api.api_v1.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class SignUpIdDto {
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$", message = "아이디는 최소 6자 이상으로, 영문자와 숫자를 포함해야 합니다.")
+    private String id;
+}
