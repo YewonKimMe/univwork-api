2e06e8a24ff25e4e7198d04b9cd1b723599c5df5
YewonKimMe <kimyewon030428@gmial.com>
2024-06-25 18:08:47 +0900

    @ToString added

--- src/main/java/net/univwork/api/api_v1/domain/dto/SignUpIdDto.java
+++ src/main/java/net/univwork/api/api_v1/domain/dto/SignUpIdDto.java
@@ -3,11 +3,14 @@ package net.univwork.api.api_v1.domain.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class SignUpIdDto {
 
     @NotBlank(message = "아이디를 입력해 주세요.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$", message = "아이디는 최소 6자 이상으로, 영문자와 숫자를 포함해야 합니다.")
     private String id;
+
 }
