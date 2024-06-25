cbdadc98c39d2013b25c6a30b956fd9fe463f03e
YewonKimMe <kimyewon030428@gmial.com>
2024-06-25 16:03:54 +0900

    이메일 발송용 host 설정 파일

---
+++ src/main/java/net/univwork/api/api_v1/config/HostConfig.java
@@ -0,0 +1,9 @@
+package net.univwork.api.api_v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/host.properties")
+public class HostConfig {
+}
