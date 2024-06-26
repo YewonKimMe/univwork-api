16e24528166794acf45c98c08aacae684fdd1b97
YewonKimMe <kimyewon030428@gmial.com>
2024-06-26 12:33:57 +0900

    유저 회원가입 기능 구현 완료.

--- src/main/java/net/univwork/api/api_v1/service/SingUpService.java
+++ src/main/java/net/univwork/api/api_v1/service/SingUpService.java
@@ -2,20 +2,22 @@ package net.univwork.api.api_v1.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.univwork.api.api_v1.domain.dto.SignUpEmailDto;
-import net.univwork.api.api_v1.domain.dto.SignUpFormDto;
-import net.univwork.api.api_v1.domain.dto.SignUpIdDto;
+import net.univwork.api.api_v1.domain.dto.*;
+import net.univwork.api.api_v1.domain.entity.Authority;
 import net.univwork.api.api_v1.domain.entity.User;
 import net.univwork.api.api_v1.enums.Role;
 import net.univwork.api.api_v1.enums.UnivEmailDomain;
 import net.univwork.api.api_v1.exception.EmailAlreadyExistException;
 import net.univwork.api.api_v1.exception.UserAlreadyExistException;
+import net.univwork.api.api_v1.exception.UserNotExistException;
+import net.univwork.api.api_v1.repository.jpa.JpaAuthorityRepository;
 import net.univwork.api.api_v1.repository.jpa.JpaUserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -29,6 +31,8 @@ public class SingUpService {
 
     private final JpaUserRepository userRepository;
 
+    private final JpaAuthorityRepository authorityRepository;
+
     private final RedisService redisService;
 
     private final PasswordEncoder passwordEncoder;
@@ -37,23 +41,36 @@ public class SingUpService {
     public void createUser(SignUpFormDto signUpFormDto) {
 
         String hashedPw = passwordEncoder.encode(signUpFormDto.getPassword());
+
         // 유저 엔티티 생성
         User user = User.builder()
                 .userId(signUpFormDto.getId())
                 .email(signUpFormDto.getEmail())
-                .pwd(hashedPw).role(Role.PREFIX.getRole() + Role.USER.getRole())
+                .pwd(hashedPw)
+                .role(Role.PREFIX.getRole() + Role.USER.getRole())
                 .createDate(new Timestamp(System.currentTimeMillis()))
                 .verification(false)
+                .authorities(new HashSet<>())
                 .build();
 
+        // Authority 생성
+        Authority authority = Authority.builder()
+                .name("ROLE_USER")
+                .build();
+
+        // 양방향 관계 설정
+        authority.setUser(user); // user와 authority 연결
+        user.getAuthorities().add(authority); // user에 authority 추가
+
         // 유저 등록
-        userRepository.save(user);
+        userRepository.save(user); // save 후에 user 객체에 자동으로 생성된 no 값이 채워짐
 
         log.info("[User create - id: {}, date: {}]", signUpFormDto.getId(), new Timestamp(System.currentTimeMillis()));
 
         // uuid 생성 후 redis 에 저장
         String authToken = UUID.randomUUID().toString();
         redisService.save(authToken, signUpFormDto.getEmail(), 48, TimeUnit.HOURS);
+
         // 이메일 발송
         emailService.sendVerifyEmail(signUpFormDto.getEmail(), authToken);
     }
@@ -95,6 +112,35 @@ public class SingUpService {
         if (findUser.isPresent()) {
             throw new UserAlreadyExistException("이미 사용중인 아이디 입니다.");
         }
+    }
+
+    public EmailVerificationDto verify(final String authToken) {
+
+        log.debug("authToken={}", authToken);
+
+        // redisService 에서 authToken 으로 이메일을 찾아오고
+        String email = redisService.find(authToken);
+
+        if (email == null) {
+            String errorMessage = "인증키가 유효하지 않거나 인증키가 만료 되었습니다.";
+
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        // 찾은 이메일로 User 의 verify = 1 설정 후, User 획득
+        User user = userRepository.findUserByEmail(email).orElseThrow(() -> new UserNotExistException("이메일로 유저를 찾지 못했습니다."));
+        user.setVerification(true);
+
+        // 획득한 User 의 로그인 아이디와 이메일을 dto에 매핑
+        EmailVerificationDto emailVerificationDto = EmailVerificationDto.builder()
+                .loginId(user.getUserId())
+                .email(user.getEmail())
+                .build();
+
+        // redis 에서 authToken 삭제
+        redisService.delete(authToken);
+
+        return emailVerificationDto;
     }
 
 }
