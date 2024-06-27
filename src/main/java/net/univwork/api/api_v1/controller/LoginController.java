17e2893d52fc813db01d7e68dcf6faab3a630c15
YewonKimMe <kimyewon030428@gmial.com>
2024-06-27 19:27:00 +0900

    로그인 실패 오류 처리

--- src/main/java/net/univwork/api/api_v1/controller/LoginController.java
+++ src/main/java/net/univwork/api/api_v1/controller/LoginController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import net.univwork.api.api_v1.domain.response.SuccessResultAndMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,10 @@ public class LoginController {
 
     @Operation(summary = "Login", description = "Basic Authentication Login end-point")
     @PostMapping
-    public ResponseEntity<SuccessResultAndMessage> login() {
-        return ResponseEntity.ok(new SuccessResultAndMessage(HttpStatus.OK.getReasonPhrase(), "로그인 되었습니다."));
+    public ResponseEntity<SuccessResultAndMessage> login(Authentication authentication) {
+        if (authentication != null) {
+            return ResponseEntity.ok(new SuccessResultAndMessage(HttpStatus.OK.getReasonPhrase(), "로그인 되었습니다."));
+        }
+        throw new BadCredentialsException("로그인 과정에서 오류가 발생했습니다.");
     }
 }
