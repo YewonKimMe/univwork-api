214cd57c1fe25b6695a170b2d06a061739c125b7
YewonKimMe <kimyewon030428@gmial.com>
2024-06-27 13:40:02 +0900

    CommentDto 변경

--- src/main/java/net/univwork/api/api_v1/domain/dto/CommentDto.java
+++ src/main/java/net/univwork/api/api_v1/domain/dto/CommentDto.java
@@ -1,14 +1,13 @@
 package net.univwork.api.api_v1.domain.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import net.univwork.api.api_v1.domain.entity.WorkplaceComment;
 import net.univwork.api.api_v1.tool.UUIDConverter;
 
 import java.sql.Timestamp;
 
-@Data
-@Builder
+@Getter
+@ToString
 public class CommentDto {
     private Long commentCode;
 
@@ -26,34 +25,11 @@ public class CommentDto {
 
     private int upvote;
 
-    private String nickname;
-
     private Timestamp timestamp;
 
     private boolean deleteFlag;
 
     private boolean reportFlag;
-
-    private String author;
-
-    public CommentDto() {
-    }
-
-    public CommentDto(Long commentCode, Long univCode, Long workplaceCode, String workplaceName, String univName, String comment_uuid, String comment, int upvote, String nickname, Timestamp timestamp, boolean deleteFlag, boolean reportFlag, String author) {
-        this.commentCode = commentCode;
-        this.univCode = univCode;
-        this.workplaceCode = workplaceCode;
-        this.workplaceName = workplaceName;
-        this.univName = univName;
-        this.comment_uuid = comment_uuid;
-        this.comment = comment;
-        this.upvote = upvote;
-        this.nickname = nickname;
-        this.timestamp = timestamp;
-        this.deleteFlag = deleteFlag;
-        this.reportFlag = reportFlag;
-        this.author = author;
-    }
 
     public CommentDto(WorkplaceComment workplaceComment) {
         this.commentCode = workplaceComment.getCommentCode();
@@ -64,10 +40,8 @@ public class CommentDto {
         this.comment_uuid = UUIDConverter.convertBinary16ToUUID(workplaceComment.getComment_uuid()).toString();
         this.comment = workplaceComment.getComment();
         this.upvote = workplaceComment.getUpvote();
-        this.nickname = workplaceComment.getNickname();
         this.timestamp = workplaceComment.getTimestamp();
         this.deleteFlag = workplaceComment.isDeleteFlag();
         this.reportFlag = workplaceComment.isReportFlag();
-        this.author = UUIDConverter.convertBinary16ToUUID(workplaceComment.getAuthor()).toString();
     }
 }
