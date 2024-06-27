f346ed57d2fb60a8c4d523596136cf51ba837665
YewonKimMe <kimyewon030428@gmial.com>
2024-06-27 18:53:39 +0900

    최초 로그인 요청이 아닌 경우에 실행되지 않음

--- src/main/java/net/univwork/api/api_v1/security/customfilter/AuthorityLoggingFilterAfter.java
+++ src/main/java/net/univwork/api/api_v1/security/customfilter/AuthorityLoggingFilterAfter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,5 +22,16 @@ public class AuthorityLoggingFilterAfter extends OncePerRequestFilter {
             log.info("[유저명: {}, 접속 시간: {}, 권한: {}]", authentication.getName(), new Timestamp(System.currentTimeMillis()), authentication.getAuthorities());
         }
         filterChain.doFilter(request, response);
+    }
+
+    // 최초 로그인 요청이 아닌 경우에 실행되지 않음
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getServletPath();
+        AntPathMatcher pathMatcher = new AntPathMatcher();
+
+        log.debug("pathMatcher?={}", !pathMatcher.match("/api/v1/login/**", path));
+
+        return !pathMatcher.match("/api/v1/login/**", path);
     }
 }
