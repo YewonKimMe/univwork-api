f2c709ecbda59979d3e95826859d6afeaa8ba689
YewonKimMe <kimyewon030428@gmial.com>
2024-06-27 18:54:35 +0900

    auth 관련 설정 추가

--- src/main/java/net/univwork/api/api_v1/tool/swagger/SwaggerConfig.java
+++ src/main/java/net/univwork/api/api_v1/tool/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package net.univwork.api.api_v1.tool.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInformation());
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                        .addSecuritySchemes("basicAuth",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")))
+                .info(apiInformation())
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
     }
 
     private Info apiInformation() {
