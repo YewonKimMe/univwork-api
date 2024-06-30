756153f3522d6cfe330969c63cc625efb6d82c29
YewonKimMe <kimyewon030428@gmial.com>
2024-06-30 11:03:44 +0900

    CustomAuthenticationEntryPoint

---
+++ src/main/java/net/univwork/api/api_v1/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package net.univwork.api.api_v1.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.domain.response.ErrorResultAndMessage;
+import net.univwork.api.api_v1.domain.response.ResultAndMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        log.debug("AuthenticationEntryPoint, unauthenticated request detected");
+        Exception e = (Exception) request.getAttribute("exception");
+        log.debug("exception={}", e.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("utf-8");
+        ResultAndMessage messageObj = new ErrorResultAndMessage(HttpStatus.UNAUTHORIZED.getReasonPhrase(), "유효하지 않은 인증 정보 입니다.");
+        String resultMessage = objectMapper.writeValueAsString(messageObj);
+        response.getWriter().write(resultMessage);
+    }
+}
