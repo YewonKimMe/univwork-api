1fce54043058f7e390d7f0de4a14ecbfecdc1c7e
YewonKimMe <kimyewon030428@gmial.com>
2024-06-30 18:15:46 +0900

    CommentReportDto

---
+++ src/main/java/net/univwork/api/api_v1/domain/dto/CommentReportDto.java
@@ -0,0 +1,12 @@
+package net.univwork.api.api_v1.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class CommentReportDto {
+    private String commentUuid;
+}
