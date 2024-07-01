4cb004c0618f6f988bfb758a61d975a3bbeefecc
YewonKimMe <kimyewon030428@gmial.com>
2024-07-01 11:34:57 +0900

    ReportedCommentRepository

---
+++ src/main/java/net/univwork/api/api_v1/repository/ReportedCommentRepository.java
@@ -0,0 +1,8 @@
+package net.univwork.api.api_v1.repository;
+
+import net.univwork.api.api_v1.domain.entity.ReportedComment;
+
+public interface ReportedCommentRepository {
+
+    void save(ReportedComment reportedCommentResult);
+}
