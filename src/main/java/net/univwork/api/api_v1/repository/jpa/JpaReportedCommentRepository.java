7cc0e2de114e175e23e6cd7098b6b9833fd02601
YewonKimMe <kimyewon030428@gmial.com>
2024-07-01 11:34:25 +0900

    JpaReportedCommentRepository

---
+++ src/main/java/net/univwork/api/api_v1/repository/jpa/JpaReportedCommentRepository.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.repository.jpa;
+
+import net.univwork.api.api_v1.domain.entity.ReportedComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaReportedCommentRepository extends JpaRepository<ReportedComment, Long> {
+}
