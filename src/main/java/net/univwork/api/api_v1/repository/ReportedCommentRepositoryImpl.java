8d662b1faab47a010220bce1e03e7888fc9511bb
YewonKimMe <kimyewon030428@gmial.com>
2024-07-01 11:35:05 +0900

    ReportedCommentRepositoryImpl

---
+++ src/main/java/net/univwork/api/api_v1/repository/ReportedCommentRepositoryImpl.java
@@ -0,0 +1,19 @@
+package net.univwork.api.api_v1.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.domain.entity.ReportedComment;
+import net.univwork.api.api_v1.repository.jpa.JpaReportedCommentRepository;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class ReportedCommentRepositoryImpl implements ReportedCommentRepository {
+
+    private final JpaReportedCommentRepository jpaReportedCommentRepository;
+    @Override
+    public void save(ReportedComment reportedComment) {
+        jpaReportedCommentRepository.save(reportedComment);
+    }
+}
