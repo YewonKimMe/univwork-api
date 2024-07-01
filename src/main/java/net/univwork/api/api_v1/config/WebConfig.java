d59c3e8f3db43337f0e4955e249a7eb3a003a56a
YewonKimMe <kimyewon030428@gmial.com>
2024-07-01 11:36:17 +0900

    WebConfig

---
+++ src/main/java/net/univwork/api/api_v1/config/WebConfig.java
@@ -0,0 +1,9 @@
+package net.univwork.api.api_v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
