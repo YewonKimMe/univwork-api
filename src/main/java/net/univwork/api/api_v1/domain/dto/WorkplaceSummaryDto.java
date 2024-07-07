db00361cf261eefef9850e46605e4d3d8e50a552
YewonKimMe <kimyewon030428@gmial.com>
2024-07-07 16:44:38 +0900

    WorkplaceSummaryDto

---
+++ src/main/java/net/univwork/api/api_v1/domain/dto/WorkplaceSummaryDto.java
@@ -0,0 +1,29 @@
+package net.univwork.api.api_v1.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class WorkplaceSummaryDto {
+
+    private Long univCode;
+
+    private Long workplaceCode;
+
+    private String univName;
+
+    private String workplaceName;
+
+    private String workType;
+
+    private String workplaceType;
+
+    private Long views;
+
+    private Long commentNum;
+}
