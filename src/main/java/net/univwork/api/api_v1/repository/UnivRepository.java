44d5fe50ace57a28545ed4d07eeddea41d53c5a7
YewonKimMe <kimyewon030428@gmial.com>
2024-07-07 16:45:32 +0900

    getWorkplacesSummary()

--- src/main/java/net/univwork/api/api_v1/repository/UnivRepository.java
+++ src/main/java/net/univwork/api/api_v1/repository/UnivRepository.java
@@ -1,6 +1,7 @@
 package net.univwork.api.api_v1.repository;
 
 import jakarta.annotation.Nullable;
+import net.univwork.api.api_v1.domain.dto.WorkplaceSummaryDto;
 import net.univwork.api.api_v1.domain.entity.University;
 import net.univwork.api.api_v1.domain.entity.Workplace;
 import net.univwork.api.api_v1.enums.SortOption;
@@ -31,4 +32,16 @@ public interface UnivRepository {
      * @since 1.0.0
      * */
     Page<Workplace> getWorkplaces(Pageable pageable, final Long univCode, @Nullable final String workplaceName, final WorkplaceType workplaceType, final SortOption sortOption);
+
+    /**
+     * 근로지 요약 리스트를 페이지로 가져오는 메소드
+     * @param pageable 페이지 객체
+     * @param univCode 학교 코드
+     * @param workplaceName 근로지 이름
+     * @param workplaceType 근로지 타입(enum)
+     * @param sortOption 정렬 옵션(enum)
+     * @apiNote 대학교 별 근로지 리스트 획득, 검색어 workplaceName 과 근로지 타입 workplaceType, sortOption 으로 정렬 및 필터링 가능
+     * @since 1.0.0
+     * */
+    Page<WorkplaceSummaryDto> getWorkplacesSummary(Pageable pageable, final Long univCode, @Nullable final String workplaceName, final WorkplaceType workplaceType, final SortOption sortOption);
 }
