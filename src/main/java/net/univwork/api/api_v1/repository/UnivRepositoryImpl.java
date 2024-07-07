17a5cf0819da94850e972206bb99815ac1dcf7f4
YewonKimMe <kimyewon030428@gmial.com>
2024-07-07 16:46:22 +0900

    getWorkplacesSummary()

--- src/main/java/net/univwork/api/api_v1/repository/UnivRepositoryImpl.java
+++ src/main/java/net/univwork/api/api_v1/repository/UnivRepositoryImpl.java
@@ -2,11 +2,13 @@ package net.univwork.api.api_v1.repository;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.domain.dto.WorkplaceSummaryDto;
 import net.univwork.api.api_v1.domain.entity.QUniversity;
 import net.univwork.api.api_v1.domain.entity.QWorkplace;
 import net.univwork.api.api_v1.domain.entity.University;
@@ -24,7 +26,7 @@ import java.util.List;
 
 @Slf4j
 @Repository
-public class UnivRepositoryImpl implements UnivRepository{
+public class UnivRepositoryImpl implements UnivRepository {
 
     private final EntityManager em;
 
@@ -150,5 +152,69 @@ public class UnivRepositoryImpl implements UnivRepository{
         // PageableExecutionUtils.getPage(): 페이지 사이즈보다 작을 경우 또는 마지막 페이지일 경우에는 추가적인 데이터베이스 쿼리를 실행하지 않음
         return PageableExecutionUtils.getPage(workplaceList, pageable,countQuery::fetchOne);
 
+    }
+
+    /**
+     * 대학 별 근로지 요약정보를 가져오는 메소드, workplaceName과 sortOption에 따라 검색 및 정렬 가능한 메소드
+     * @param pageable pageable 객체(서비스 계층에서 구현)
+     * @param univCode 학교 코드
+     * @param workplaceName @Nullable 근로지명(검색 옵션)
+     * @param sortOption 정렬 조건 enum
+     * @return Workplace 페이지 객체
+     * @since 1.0.0
+     * */
+    @Override
+    public Page<WorkplaceSummaryDto> getWorkplacesSummary(Pageable pageable, Long univCode, @Nullable String workplaceName, WorkplaceType workplaceType, SortOption sortOption) {
+
+        QWorkplace workplace = QWorkplace.workplace;
+        BooleanBuilder builder = new BooleanBuilder(); // 조건
+        OrderSpecifier<?> orderSpecifier = null; // 정렬
+
+        // 근로지명 검색어가 존재할 경우
+        if (StringUtils.hasText(workplaceName)) {
+            builder.and(workplace.workplaceName.like("%" + workplaceName + "%"));
+        }
+
+        // 학교 코드 where 조건 설정
+        builder.and(workplace.univCode.eq(univCode));
+
+        // 근로지 종류 지정
+        if (workplaceType == WorkplaceType.IN) {
+            builder.and(workplace.workplaceType.eq(ConstString.WORKPLACE_TYPE_IN));
+        } else if (workplaceType == WorkplaceType.OUT) {
+            builder.and(workplace.workplaceType.eq(ConstString.WORKPLACE_TYPE_OUT));
+        }
+
+        // 정렬 옵션 지정
+        if (sortOption == SortOption.WORKPLACE_NAME_ASC) { // 정렬 옵션이 근로지명 오름차순인 경우
+            orderSpecifier = workplace.workplaceName.asc();
+        } else if (sortOption == SortOption.WORKPLACE_NAME_DESC) {
+            orderSpecifier = workplace.workplaceName.desc();
+        } else if (sortOption == SortOption.WORKPLACE_VIEW_ASC) {
+            orderSpecifier = workplace.views.asc();
+        } else if (sortOption == SortOption.WORKPLACE_VIEW_DESC) {
+            orderSpecifier = workplace.views.desc();
+        } else if (sortOption == SortOption.WORKPLACE_COMMENT_NUM_ASC) {
+            orderSpecifier = workplace.commentNum.asc();
+        } else if (sortOption == SortOption.WORKPLACE_COMMENT_NUM_DESC) {
+            orderSpecifier = workplace.commentNum.desc();
+        }
+
+        // 조건에 맞는 workplace를 가져옴
+        List<WorkplaceSummaryDto> workplaceSummaryList = queryFactory
+                .select(Projections.constructor(WorkplaceSummaryDto.class, workplace.univCode, workplace.workplaceCode, workplace.univName, workplace.workplaceName, workplace.workType, workplace.workplaceType, workplace.views, workplace.commentNum))
+                .from(workplace)
+                .where(builder)
+                .orderBy(orderSpecifier)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 조건에 맞는 결과에 해당하는 총 workplace 갯수를 가져오기 위한 쿼리: SELECT count(*)
+        JPAQuery<Long> countQuery = queryFactory
+                .select(workplace.count())
+                .from(workplace)
+                .where(builder);
+        return PageableExecutionUtils.getPage(workplaceSummaryList, pageable,countQuery::fetchOne);
     }
 }
