b6f52a0eeabbe92c266d7275389345b34c0cf3b0
YewonKimMe <kimyewon030428@gmial.com>
2024-07-10 06:29:48 +0900

    PathVariable added, url alter

--- src/main/java/net/univwork/api/api_v1/controller/CommentController.java
+++ src/main/java/net/univwork/api/api_v1/controller/CommentController.java
@@ -2,6 +2,7 @@ package net.univwork.api.api_v1.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +20,23 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Comment", description = "댓글 관련 부가 기능 엔드포인트")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/comment")
+@RequestMapping("/api/v1/comment")
 public class CommentController {
 
     private final CommentService commentService;
 
     @Operation(summary = "댓글 신고", description = "특정 UUID 댓글 신고")
-    @PatchMapping
+    @PatchMapping("/{comment-uuid}")
     public ResponseEntity<ResultAndMessage> reportComments(
+            @Parameter(name = "comment-uuid", description = "댓글 UUID", in = ParameterIn.PATH) @PathVariable(name = "comment-uuid") final String commentUuid,
             @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "댓글 고유 식별자(UUID), 사유(REASON_욕설, 개인정보유포, 명예훼손, 사칭, 광고) 중 하나, 정확하게 입력하여야 함.") @RequestBody final CommentReportDto dto,
             @Parameter(hidden = true) HttpServletRequest request,
             @Parameter(hidden = true) Authentication authentication
     ) {
+        if (!commentUuid.equals(dto.getCommentUuid())) {
+            log.debug("UUID 불일치");
+            throw new IllegalArgumentException("잘못된 접근입니다.");
+        }
         commentService.reportComment(dto, request, authentication);
         return ResponseEntity.ok()
                 .body(new SuccessResultAndMessage(HttpStatus.OK.getReasonPhrase(), "댓글 신고가 완료되었습니다."));
