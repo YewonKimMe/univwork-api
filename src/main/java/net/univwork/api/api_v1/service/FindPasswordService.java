6abac227b7d15bec8cf35e365265a5ec207d6e8b
YewonKimMe <kimyewon030428@gmial.com>
2024-07-17 06:47:21 +0900

    FindPasswordService

--- src/main/java/net/univwork/api/api_v1/service/FindPasswordService.java
+++ src/main/java/net/univwork/api/api_v1/service/FindPasswordService.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
