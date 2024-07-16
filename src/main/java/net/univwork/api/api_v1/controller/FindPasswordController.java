d3daceb9fd10985b57a3ff147791f2b770093cf6
YewonKimMe <kimyewon030428@gmial.com>
2024-07-17 06:48:07 +0900

    FindPasswordController

--- src/main/java/net/univwork/api/api_v1/controller/FindPasswordController.java
+++ src/main/java/net/univwork/api/api_v1/controller/FindPasswordController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
