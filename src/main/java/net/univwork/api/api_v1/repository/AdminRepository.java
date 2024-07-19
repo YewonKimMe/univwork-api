d56ba5c459c89009c1edd55043bf081011540dc1
YewonKimMe <kimyewon030428@gmial.com>
2024-07-19 14:45:12 +0900

    AdminRepository

---
+++ src/main/java/net/univwork/api/api_v1/repository/AdminRepository.java
@@ -0,0 +1,19 @@
+package net.univwork.api.api_v1.repository;
+
+import net.univwork.api.api_v1.domain.dto.NoticeAdminDto;
+import net.univwork.api.api_v1.domain.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AdminRepository {
+
+    Notice getNotice(Long no);
+
+    void saveNotice(Notice notice);
+
+    void updateNotice(NoticeAdminDto notice, Long no);
+
+    void deleteNotice(Long no);
+
+    Page<Notice> getNoticeList(Pageable pageable);
+}
