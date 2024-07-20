aa6b5a540379c81ccbbdfea5aeccb84846bc2bd5
YewonKimMe <kimyewon030428@gmial.com>
2024-07-20 09:05:11 +0900

    info log, Auth parameter added

--- src/main/java/net/univwork/api/api_v1/service/AdminService.java
+++ src/main/java/net/univwork/api/api_v1/service/AdminService.java
@@ -9,6 +9,7 @@ import net.univwork.api.api_v1.tool.UUIDConverter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +28,7 @@ public class AdminService {
         Pageable pageable = PageRequest.of(pageNumber, pageLimit);
         return adminRepository.getNoticeList(pageable);
     }
-    public void saveNotice(NoticeAdminDto noticeAdminDto) {
+    public void saveNotice(NoticeAdminDto noticeAdminDto, Authentication authentication) {
         Notice notice = Notice.builder()
                 .noticeId(UUIDConverter.convertUuidStringToBinary16(UUID.randomUUID().toString()))
                 .title(noticeAdminDto.getTitle())
@@ -41,16 +42,19 @@ public class AdminService {
                 .isDeleted(false)
                 .build();
         adminRepository.saveNotice(notice);
+        log.info("[Notice saved] Writer: {}", authentication.getName());
     }
     public Notice getNotice(Long no) {
         return adminRepository.getNotice(no);
     }
-    public void editAndSaveNotice(NoticeAdminDto noticeAdminDto, Long no) {
+    public void editAndSaveNotice(NoticeAdminDto noticeAdminDto, Long no, Authentication authentication) {
         adminRepository.updateNotice(noticeAdminDto, no);
+        log.info("[Notice edited] Editor: {}", authentication.getName());
     }
 
-    public void deleteNotice(Long no) {
+    public void deleteNotice(Long no, Authentication authentication) {
         // isDeleted flag = 1
         adminRepository.deleteNotice(no);
+        log.info("[Notice deleted] Admin: {}", authentication.getName());
     }
 }
