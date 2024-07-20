9a0b9d4a5183f2d98dc26a39602d57f12405efb9
YewonKimMe <kimyewon030428@gmial.com>
2024-07-20 15:05:48 +0900

    uuidDecodeToByteArray

--- src/main/java/net/univwork/api/api_v1/tool/UUIDConverter.java
+++ src/main/java/net/univwork/api/api_v1/tool/UUIDConverter.java
@@ -3,6 +3,7 @@ package net.univwork.api.api_v1.tool;
 import org.springframework.stereotype.Component;
 
 import java.nio.ByteBuffer;
+import java.util.Base64;
 import java.util.UUID;
 
 @Component
@@ -39,5 +40,11 @@ public class UUIDConverter {
         long leastSignificantBits = byteBuffer.getLong();
 
         return new UUID(mostSignificantBits, leastSignificantBits);
+    }
+
+    // base64 인코딩된 문자열을 byte[] 배열로 변환,
+    // 클라이언트에서 base64 encoded String 을 보냈을 때 바로 사용 가능하게
+    public static byte[] uuidDecodeToByteArray(String encodedUuid) {
+        return Base64.getDecoder().decode(encodedUuid);
     }
 }
