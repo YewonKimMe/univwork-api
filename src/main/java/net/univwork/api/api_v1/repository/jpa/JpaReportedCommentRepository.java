6c7482d0b96183dc956b2f435e9d45c58c0c8879
YewonKimMe <kimyewon030428@gmial.com>
2024-07-21 10:48:09 +0900

    findReportedCommentByCommentUuid

--- src/main/java/net/univwork/api/api_v1/repository/jpa/JpaReportedCommentRepository.java
+++ src/main/java/net/univwork/api/api_v1/repository/jpa/JpaReportedCommentRepository.java
@@ -4,4 +4,6 @@ import net.univwork.api.api_v1.domain.entity.ReportedComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaReportedCommentRepository extends JpaRepository<ReportedComment, Long> {
+
+    ReportedComment findReportedCommentByCommentUuid(byte[] commentUuid);
 }
