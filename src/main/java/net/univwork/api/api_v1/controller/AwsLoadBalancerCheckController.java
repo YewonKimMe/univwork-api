925f10b904b92fe1b0cbd29b1e7810495c799cfc
YewonKimMe <kimyewon030428@gmial.com>
2024-07-25 00:34:07 +0900

    AwsLoadBalancerCheckController

---
+++ src/main/java/net/univwork/api/api_v1/controller/AwsLoadBalancerCheckController.java
@@ -0,0 +1,22 @@
+package net.univwork.api.api_v1.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Tag(name = "AWS Health Check", description = "AWS 로드 밸런서 상태 체크")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/aws/health-check")
+public class AwsLoadBalancerCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok().body("OK");
+    }
+}
