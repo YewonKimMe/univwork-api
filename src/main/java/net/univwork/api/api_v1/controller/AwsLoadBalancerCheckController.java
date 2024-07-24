71a411b2715703ac8b41e4fe07850eb9f5bc12e7
YewonKimMe <kimyewon030428@gmial.com>
2024-07-25 01:37:50 +0900

    alter produces

--- src/main/java/net/univwork/api/api_v1/controller/AwsLoadBalancerCheckController.java
+++ src/main/java/net/univwork/api/api_v1/controller/AwsLoadBalancerCheckController.java
@@ -3,6 +3,7 @@ package net.univwork.api.api_v1.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "AWS Health Check", description = "AWS 로드 밸런서 상태 체크")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/aws/health-check")
+@RequestMapping(value = "/api/v1/aws/health-check", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AwsLoadBalancerCheckController {
 
     @GetMapping
