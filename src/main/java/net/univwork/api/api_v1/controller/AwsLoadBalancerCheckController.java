15a0243dadfc63f9625d2b00375afe1143c07ea8
YewonKimMe <kimyewon030428@gmial.com>
2024-07-25 01:43:53 +0900

    aws health check

--- src/main/java/net/univwork/api/api_v1/controller/AwsLoadBalancerCheckController.java
+++ src/main/java/net/univwork/api/api_v1/controller/AwsLoadBalancerCheckController.java
@@ -1,8 +1,12 @@
 package net.univwork.api.api_v1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.univwork.api.api_v1.domain.response.ResultAndMessage;
+import net.univwork.api.api_v1.domain.response.SuccessResultAndMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/aws/health-check", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AwsLoadBalancerCheckController {
 
+    @Operation(summary = "aws", description = "aws health check")
     @GetMapping
-    public ResponseEntity<String> checkHealth() {
-        return ResponseEntity.ok().body("OK");
+    public ResponseEntity<ResultAndMessage> checkHealth() {
+        return ResponseEntity.ok().body(new SuccessResultAndMessage(HttpStatus.OK.getReasonPhrase(), "OK"));
     }
 }
