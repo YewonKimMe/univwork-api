1bc8303199f9fd122108589c508788e2e4a987df
YewonKimMe <kimyewon030428@gmial.com>
2024-07-24 14:53:58 +0900

    PROD 오류 수정

--- src/main/java/net/univwork/api/api_v1/security/SecurityConfig.java
+++ src/main/java/net/univwork/api/api_v1/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                                 CorsConfiguration config = new CorsConfiguration();
                                 config.setAllowedOrigins(Arrays.asList(
                                         devCorsAllowedUrl,
-                                        devCorsAllowedUrl
+                                        prodCorsAllowedUrl
                                 ));
                                 config.setAllowedMethods(Collections.singletonList("*"));
                                 config.setAllowCredentials(true);
