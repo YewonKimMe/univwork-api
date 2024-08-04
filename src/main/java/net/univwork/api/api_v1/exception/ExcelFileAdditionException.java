549cae45481d87be1c3e643db47f11a234621ca6
YewonKimMe <kimyewon030428@gmial.com>
2024-08-05 03:25:10 +0900

    ExcelFileAdditionException

---
+++ src/main/java/net/univwork/api/api_v1/exception/ExcelFileAdditionException.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.exception;
+
+public class ExcelFileAdditionException extends RuntimeException {
+    public ExcelFileAdditionException(String message) {
+        super(message);
+    }
+}
