c861781ea16d45018d3a78133c9a409d512fc578
YewonKimMe <kimyewon030428@gmial.com>
2024-08-05 03:26:35 +0900

    AddWorkplaceDto

---
+++ src/main/java/net/univwork/api/api_v1/domain/dto/AddWorkplaceDto.java
@@ -0,0 +1,35 @@
+package net.univwork.api.api_v1.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class AddWorkplaceDto {
+
+    private Long univCode;
+
+    private String workType;
+
+    private String workplaceType;
+
+    private String workplaceName;
+
+    private String workplaceAddress;
+
+    private String workTime;
+
+    private String workDay;
+
+    private String requiredNum;
+
+    private String preferredDepartment;
+
+    private String preferredGrade;
+
+    private String jobDetail;
+
+    private String note;
+}
