fed5c49a9c869708f1df7346a518996cc4de2ce2
YewonKimMe <kimyewon030428@gmial.com>
2024-08-07 12:07:49 +0900

    log-username, ip

--- src/main/java/net/univwork/api/api_v1/security/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/net/univwork/api/api_v1/security/CustomBasicAuthenticationEntryPoint.java
@@ -6,13 +6,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import net.univwork.api.api_v1.domain.response.ErrorResultAndMessage;
 import net.univwork.api.api_v1.domain.response.ResultAndMessage;
+import net.univwork.api.api_v1.tool.IpTool;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @Component
@@ -27,8 +33,24 @@ public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntr
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        String requestIp = IpTool.getIpAddr(request);
+        // Basic Authentication Token 에서 Username 획득
+        String username = "Anonymous";
+        String base64Credentials = null;
+        String decodedCredentials = null;
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (null != authorizationHeader) {
+            base64Credentials = authorizationHeader.substring("Basic ".length());
+            byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+            decodedCredentials = new String(decodedBytes, StandardCharsets.UTF_8);
+            username = decodedCredentials.substring(0, decodedCredentials.indexOf(":"));
+        }
+
+
         log.debug("AuthenticationEntryPoint, unauthenticated request detected");
-        log.info("AuthenticationEntryPoint, unauthenticated request detected, path={}", request.getServletPath());
+        log.info("[AuthenticationEntryPoint] unauthenticated request detected, Path={}, Username={}, IP-Address={}", request.getServletPath(), username, requestIp);
         Exception e = (Exception) request.getAttribute("exception");
         if (e != null) {
             log.debug("exception: {}", e.getMessage());
