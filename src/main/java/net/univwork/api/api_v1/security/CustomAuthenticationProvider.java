c99b0cf3fe472fe888c01de3e8e9af46cbca1a00
YewonKimMe <kimyewon030428@gmial.com>
2024-08-08 02:39:14 +0900

    로그인 시 인증되지 않은 회원 재인증 요구

--- src/main/java/net/univwork/api/api_v1/security/CustomAuthenticationProvider.java
+++ src/main/java/net/univwork/api/api_v1/security/CustomAuthenticationProvider.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.univwork.api.api_v1.domain.entity.Authority;
 import net.univwork.api.api_v1.domain.entity.User;
-import net.univwork.api.api_v1.exception.BlockedClientException;
 import net.univwork.api.api_v1.repository.jpa.JpaUserRepository;
+import net.univwork.api.api_v1.service.EmailService;
+import net.univwork.api.api_v1.service.RedisService;
+import net.univwork.api.api_v1.tool.TimestampTool;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,10 +18,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,7 +32,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final RedisService redisService;
+
+    private final EmailService emailService;
+
     private final String badCredentialsMessage = "아이디 또는 비밀번호를 확인해 주세요.";
+
+    private final String validateValue = "NEED_VERIFY";
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -50,6 +58,26 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         log.debug("findUser={}", user.toString());
         if (!user.isVerification()) {
             log.debug("이메일 인증이 필요합니다. 아이디={}", username);
+            log.info("[이메일 인증 필요] 이메일 인증이 필요한 계정, 아이디={}", username);
+            String findVerifyValue = redisService.find(user.getEmail());
+            log.debug("findVerifyValue={}", findVerifyValue);
+            if (findVerifyValue == null) {
+                Timestamp createDate = user.getCreateDate();
+                Timestamp now = Timestamp.from(Instant.now());
+                if (TimestampTool.calculateHowMillisecondsDiffEndToStart(createDate, now, TimeUnit.HOURS.toMillis(24))) {
+                    log.info("[가입 후 24시간 경과한 유저] 인증메일 재발송, Username={}", username);
+                    int time = 3;
+                    String authToken = UUID.randomUUID().toString();
+                    redisService.saveIfAbsent(authToken, user.getEmail(), time, TimeUnit.HOURS);
+                    emailService.sendVerifyEmail(user.getEmail(), authToken, time);
+
+                    // 중복 발송 방지를 위해서 email 을 redis 에 key 로 저장
+                    redisService.saveIfAbsent(user.getEmail(), validateValue, time, TimeUnit.HOURS);
+                    throw new BadCredentialsException("등록 이후 24시간 경과 후에도 이메일 계정이 인증되지 않았습니다.\n가입 시 등록한 학교 이메일 주소로 인증 메일이 재발송 되었습니다.");
+                }
+            }else if (findVerifyValue.equals(validateValue)) {
+                throw new BadCredentialsException("최초 이메일 인증 이후에 로그인이 가능합니다.\n추가 인증 메일이 이미 발송되었습니다.\n이메일 발송 까지 최대 5분이 소요될 수 있습니다.\n이메일이 도착하지 않았다면 관리자에게 문의해 주세요.");
+            }
             throw new BadCredentialsException("최초 이메일 인증 이후에 로그인이 가능합니다.");
         }
         if (user.isBlockedFlag()) {
