1aff88db3218a2a3d6a15357beccb23202e257de
YewonKimMe <kimyewon030428@gmial.com>
2024-08-08 00:28:25 +0900

    timezone setting

--- src/main/java/net/univwork/api/ApiApplication.java
+++ src/main/java/net/univwork/api/ApiApplication.java
@@ -1,13 +1,22 @@
 package net.univwork.api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// JVM 기본 시간대를 한국 시간대(KST)로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
