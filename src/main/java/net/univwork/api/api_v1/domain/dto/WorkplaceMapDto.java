fcfd1c7fab6fd2461cdc63b70d0a6c67eb1ffaf4
YewonKimMe <kimyewon030428@gmial.com>
2024-08-12 05:36:44 +0900

    WorkplaceMapDto

---
+++ src/main/java/net/univwork/api/api_v1/domain/dto/WorkplaceMapDto.java
@@ -0,0 +1,33 @@
+package net.univwork.api.api_v1.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class WorkplaceMapDto {
+
+    private Long workplaceCode;
+
+    private Long univCode;
+
+    private String univName;
+
+    private String workType;
+
+    private String workplaceType;
+
+    private String workplaceName;
+
+    private String workplaceAddress;
+
+    private Double rating;
+
+    private Double lat;
+
+    private Double lng;
+}
