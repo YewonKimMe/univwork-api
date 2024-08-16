caf7c340d8e6a541fa7618e51d6d452f9776fed4
YewonKimMe <kimyewon030428@gmial.com>
2024-08-17 07:35:53 +0900

    saveWorkplaceComment 임시 커밋

--- src/main/java/net/univwork/api/api_v1/service/WorkplaceService.java
+++ src/main/java/net/univwork/api/api_v1/service/WorkplaceService.java
@@ -1,5 +1,8 @@
 package net.univwork.api.api_v1.service;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.univwork.api.api_v1.domain.dto.CommentDto;
@@ -9,10 +12,16 @@ import net.univwork.api.api_v1.domain.dto.WorkplaceRatingDto;
 import net.univwork.api.api_v1.domain.entity.University;
 import net.univwork.api.api_v1.domain.entity.Workplace;
 import net.univwork.api.api_v1.domain.entity.WorkplaceComment;
+import net.univwork.api.api_v1.enums.CookieName;
 import net.univwork.api.api_v1.enums.SortOption;
 import net.univwork.api.api_v1.enums.WorkplaceType;
 import net.univwork.api.api_v1.exception.DomainNotMatchException;
+import net.univwork.api.api_v1.exception.DuplicationException;
+import net.univwork.api.api_v1.exception.NoAuthenticationException;
+import net.univwork.api.api_v1.exception.NoCookieValueException;
 import net.univwork.api.api_v1.repository.WorkplaceRepository;
+import net.univwork.api.api_v1.tool.CookieUtils;
+import net.univwork.api.api_v1.tool.IpTool;
 import net.univwork.api.api_v1.tool.UUIDConverter;
 import net.univwork.api.api_v1.tool.UserInputXSSGuard;
 import org.springframework.data.domain.Page;
@@ -24,7 +33,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -63,7 +75,7 @@ public class WorkplaceService {
      * @return CommentDto 페이지 객체
      * @see net.univwork.api.api_v1.repository.WorkplaceRepository#getWorkplaceComments(Pageable, Long, Long)
      * */
-    public Page<CommentDto> getWorkplaceComments(final Long univCode, final Long workplaceCode, final int pageNumber, final int pageLimit, Authentication authentication) {
+    public Page<CommentDto> getWorkplaceComments(final Long univCode, final Long workplaceCode, final int pageNumber, final int pageLimit, Authentication authentication, final boolean isAllowAnonymousUsers) {
 
         Pageable pageable = PageRequest.of(pageNumber, pageLimit); // pageable 객체 생성
 
@@ -73,8 +85,10 @@ public class WorkplaceService {
         List<CommentDto> commentDtoList = workplaceComments.getContent().stream()
                 .map(comment -> {
                     CommentDto dto = new CommentDto(comment);
-                    if (authentication == null || !authentication.isAuthenticated()) {
-                        dto.setComment("로그인 후 근로지 댓글을 확인하실 수 있습니다.");
+                    if (!isAllowAnonymousUsers) {
+                        if (authentication == null || !authentication.isAuthenticated()) {
+                            dto.setComment("로그인 후 근로지 댓글을 확인하실 수 있습니다.");
+                        }
                     }
                     return dto;
                 })
@@ -101,17 +115,77 @@ public class WorkplaceService {
      * @return CommentDto 페이지 객체
      * @see net.univwork.api.api_v1.repository.WorkplaceRepository#saveWorkplaceComment(WorkplaceComment) 
      * */
-    public CommentDto saveWorkplaceComment(CommentFormDto commentFormDto, final Long univCode, final Long workplaceCode, final String userIp, final Authentication authentication) {
+    public CommentDto saveWorkplaceComment(CommentFormDto commentFormDto, final Long univCode, final Long workplaceCode, final String userIp, final Authentication authentication, boolean isAllowAnonymousUsers, HttpServletRequest request, HttpServletResponse response) {
+
+        String commentCheckCookieBas64 = CookieUtils.getUserCookie(request, CookieName.WORKPLACE_COMMENT_COOKIE);
+
+        String decodedCommentCheckCookie = null;
+
+        String responseCommentCookie = null;
+
+        StringBuilder beforeEncodedSb = new StringBuilder();
+
+        boolean isAuthenticated = authentication != null;
+
+        String ipAddr = IpTool.getIpAddr(request);
+        if (ipAddr.contains(",")) {
+            ipAddr = ipAddr.split(",")[0];
+        }
+
+        // 익명유저 댓글 등록이 허용되지 않는 경우
+        if (!isAllowAnonymousUsers) {
+            if (!isAuthenticated) { // 인증 정보가 없을 경우 댓글 작성 불가능
+                throw new NoAuthenticationException("로그인 정보가 없습니다.");
+            }
+
+            if (this.countUserComments(authentication, univCode, workplaceCode) > 0) { // 같은 근로지 중복 작성 방지
+                throw new DuplicationException("이미 같은 근로지에 작성한 댓글이 존재합니다.\n근로지 당 하나의 댓글만 작성할 수 있습니다.");
+            }
+        }
+
+        // 로그인 유저가 학교, 유저 획득 후, domain 이 다르면 예외 발생 처리
+        if (isAuthenticated) {
+            University univ = univService.getUniv(univCode);
+
+            UserDetailDto userDto = userService.findUserById(authentication.getName());
+
+            if (!univ.getDomain().equals(userDto.getDomain())) {
+                throw new DomainNotMatchException("재학중인 대학 근로지에만 댓글 작성이 가능합니다.");
+            }
+        }
+
+        // 익명 유저 댓글 등록 허용, 로그인 유저도 확인
+        // commentCheckCookie 가 null 인 경우, 오류
+        if (commentCheckCookieBas64 == null) {
+            log.error("[댓글 등록 쿠키값 위변조:제거됨] ip={}", ipAddr);
+            throw new NoCookieValueException("잘못된 요청입니다.");
+        }
+
+        // commentCheckCookie 가 빈 문자열이 아닌 경우, 익명 유저 중복 방지 댓글 확인 시작
+        // 댓글 등록 확인자는 대학코드:근로지코드;대학코드:근로지코드; 로 구성되어 있음
+        if (!commentCheckCookieBas64.isEmpty() && !isAuthenticated) {
+            byte[] decodedCommentCheckCookieBytes = Base64.getDecoder().decode(commentCheckCookieBas64);
+            decodedCommentCheckCookie = new String(decodedCommentCheckCookieBytes);
+            // 응답값 StringBuilder 에 기존 쿠키값 세팅
+            beforeEncodedSb.append(decodedCommentCheckCookie);
+
+            String[] cookieArray = decodedCommentCheckCookie.split(";");
+            Arrays.stream(cookieArray).forEach(
+                    cookieValue -> {
+                        Long cookieUnivCode = Long.valueOf(cookieValue.split(":")[0]);
+                        Long cookieWorkplaceCode = Long.valueOf(cookieValue.split(":")[1]);
+                        if (univCode.equals(cookieUnivCode) && workplaceCode.equals(cookieWorkplaceCode)) {
+                            throw new DuplicationException("이미 댓글을 작성한 근로지 입니다.");
+                        }
+                    }
+            );
+            // TODO checkCommentCookie Setting
+            beforeEncodedSb.append(univCode).append(":").append(workplaceCode).append(";");
+        }
+
 
         // 근로지 우선 획득(학교 이름 필요)
         Workplace findWorkplace = getWorkplace(univCode, workplaceCode);
-
-        // 학교, 유저 획득 후, domain 이 다르면 예외 발생 처리
-        University univ = univService.getUniv(univCode);
-        UserDetailDto userDto = userService.findUserById(authentication.getName());
-        if (!univ.getDomain().equals(userDto.getDomain())) {
-            throw new DomainNotMatchException("재학중인 대학 근로지에만 댓글 작성이 가능합니다.");
-        }
 
         // commentUUID를 바이트배열로 변경
         byte[] commentUuidByte = UUIDConverter.convertUuidToBinary16();
@@ -125,7 +199,8 @@ public class WorkplaceService {
                 .univName(findWorkplace.getUnivName())
                 .commentUuid(commentUuidByte)
                 .comment(xssGuard.process(commentFormDto.getComment()))
-                .userId(authentication.getName())
+                // TODO 유저이름 세팅-uuid로 바꾸기? 프론트에서 uuid 세팅해줘야 할듯
+                .userId(isAuthenticated ? authentication.getName() : userIp)
                 .timestamp(new Timestamp(System.currentTimeMillis()))
                 .deleteFlag(false)
                 .reportFlag(false)
@@ -143,6 +218,15 @@ public class WorkplaceService {
         // workplace rating update
         this.updateWorkplaceRating(univCode, workplaceCode, findWorkplace);
 
+        if (!beforeEncodedSb.toString().isEmpty()) {
+            byte[] afterCookieByte = beforeEncodedSb.toString().getBytes();
+            responseCommentCookie = Base64.getEncoder().encodeToString(afterCookieByte);
+
+            Cookie cookie = new Cookie(CookieName.WORKPLACE_COMMENT_COOKIE.getCookieName(), responseCommentCookie);
+            cookie.setMaxAge((int) TimeUnit.DAYS.toSeconds(30));
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
         // CommentDto로 변환해서 반환
         return new CommentDto(comment); // 나중에 MapStruct 쓰자
     }
