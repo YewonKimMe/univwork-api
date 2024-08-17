33b232f190b7bf6c9ed1003ba211e660a2f67fa1
YewonKimMe <kimyewon030428@gmial.com>
2024-08-17 09:05:32 +0900

    NoCookieValueException

---
+++ src/main/java/net/univwork/api/api_v1/exception/NoCookieValueException.java
@@ -0,0 +1,7 @@
+package net.univwork.api.api_v1.exception;
+
+public class NoCookieValueException extends RuntimeException {
+    public NoCookieValueException(String message) {
+        super(message);
+    }
+}
