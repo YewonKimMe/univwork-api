535329a4e3e3af9a43481a8ce0e3cba986b055d4
YewonKimMe <kimyewon030428@gmial.com>
2024-08-24 17:42:37 +0900

    likeComment, checkDuplicatedLikeToIp, saveCommentLog

--- src/main/java/net/univwork/api/api_v1/repository/CommentRepository.java
+++ src/main/java/net/univwork/api/api_v1/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package net.univwork.api.api_v1.repository;
 
+import net.univwork.api.api_v1.domain.entity.CommentLikeLog;
 import net.univwork.api.api_v1.domain.entity.WorkplaceComment;
 
 import java.util.Optional;
@@ -7,4 +8,10 @@ import java.util.Optional;
 public interface CommentRepository {
 
     Optional<WorkplaceComment> findWorkplaceCommentByCommentUuid(byte[] uuid);
+
+    void likeComment(Long commentCode);
+
+    boolean checkDuplicatedLikeToIp(Long commentCode, String userIp);
+
+    void saveCommentLog(CommentLikeLog commentLikeLog);
 }
