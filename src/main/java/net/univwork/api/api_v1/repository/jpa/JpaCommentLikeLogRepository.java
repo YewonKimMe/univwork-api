a9f63ad6e5ca846fe8fb3dff1336d97bd075655d
YewonKimMe <kimyewon030428@gmial.com>
2024-08-24 18:40:07 +0900

    한 댓글에 여러 좋아요 -> Optional<List<CommentLikeLog>>

--- src/main/java/net/univwork/api/api_v1/repository/jpa/JpaCommentLikeLogRepository.java
+++ src/main/java/net/univwork/api/api_v1/repository/jpa/JpaCommentLikeLogRepository.java
@@ -3,9 +3,10 @@ package net.univwork.api.api_v1.repository.jpa;
 import net.univwork.api.api_v1.domain.entity.CommentLikeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaCommentLikeLogRepository extends JpaRepository<CommentLikeLog, Long> {
 
-    Optional<CommentLikeLog> findCommentLikeLogByCommentCode(Long commentCode);
+    Optional<List<CommentLikeLog>> findCommentLikeLogByCommentCode(Long commentCode);
 }
